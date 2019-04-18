a9c4ec31cfde59f8124fea92822c38078291a9c6
Jaspar <guiltylogik@gmail.com>
2019-04-17 18:26:11 -0700

    minor fixes

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -8,6 +8,11 @@ class DashboardController extends Controller
 {
     //
 
+    public function __construct()
+    {
+        $this->middleware('auth');
+    }
+
     public function home(){
 
         return view('dashboard');
